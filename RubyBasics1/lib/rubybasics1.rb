ce4dfdd8c4b294b69fc53c7e639154f2a1b8c5ed
Negina <nazizi1@uncc.edu>
2020-04-07 06:26:25 +0000

    Add comments to the show view- part 2 to show link

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -7,7 +7,18 @@ end
 
 # Part II
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty 
+    return 0
+  end
+  if (arr.length == 1)
+    return arr[0]
+  end
+  
+  large1 = arr[0]
+  large2 = arr[0]
+  
+  for (i=0; i<)
+    
 end
 
 # Part III
--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -1,10 +1,33 @@
 # RubyHashes
 # Part I
 def array_2_hash emails, contacts
-    # YOUR CODE HERE
-end
+    #moves through emails using i and goes up 1 each iteration 
+    i = 0 
+    if emails[i] == nil 
+        return contacts
+    else 
+        contacts.each_key do |k|
+        contacts[k] = emails[i]
+        i +=1
+        end
+    end
+    return contacts
+end 
 
 # Part II
 def array2d_2_hash contact_info, contacts
-    # YOUR CODE HERE
+    i = 0
+    #checks for the empty array
+    if contact_info[i].length == 0
+        return contacts
+    else 
+        contacts.each_key do |k|
+            #inner hash using test cases
+            h = {'email' => contact_info[i][0], 'phone' =>contact_info[i][1]}
+            contacts[k] = h
+            #iterates through
+            i += 1
+        end
+    end
+        return contacts
 end
